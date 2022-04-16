d2cde6939050546bfdc14518b172f446a2abfaf5
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2022-04-16 14:42:25 +0600

    home listview added

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -17,7 +17,9 @@ class HomeScreen extends StatelessWidget {
             Align(
               alignment: Alignment.topCenter,
               child: Container(
-                color: Get.isDarkMode ? Theme.of(context).cardColor : kPrimaryColor,
+                color: Get.isDarkMode
+                    ? Theme.of(context).cardColor
+                    : kPrimaryColor,
                 //color: kPrimaryColor,
                 height: 100,
                 width: SizeConfig.screenWidth,
@@ -25,25 +27,27 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 10),
-                      child: IconButton(onPressed: (){}, icon: SizedBox(
-                        width: 20,
-                        child: SvgPicture.asset(
-                          Images.review,
-                          color: Colors.white,
-                        ),
-                      )),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: SizedBox(
+                            width: 20,
+                            child: SvgPicture.asset(
+                              Images.review,
+                              color: Colors.white,
+                            ),
+                          )),
                     ),
                     Positioned(
-                      top: 10,
+                        top: 10,
                         left: 0,
                         right: 0,
                         child: SizedBox(
-                      height: 35,
-                      child: Image.asset(
-                        Images.homeLogo,
-                        color: Colors.white,
-                      ),
-                    )),
+                          height: 35,
+                          child: Image.asset(
+                            Images.homeLogo,
+                            color: Colors.white,
+                          ),
+                        )),
                   ],
                 ),
               ),
@@ -53,20 +57,25 @@ class HomeScreen extends StatelessWidget {
               width: SizeConfig.screenWidth,
               height: 200,
               decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-                borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(15.0),
-                    topLeft: Radius.circular(15.0)),
-                shape: BoxShape.rectangle
-              ),
+                  color: Theme.of(context).backgroundColor,
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(15.0),
+                      topLeft: Radius.circular(15.0)),
+                  shape: BoxShape.rectangle),
               child: SingleChildScrollView(
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Column(
-                    children: [
-                      Text('fdaljdf'),
-                    ],
-                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: ListView.builder(
+                      itemCount: 10,
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                    return Column(
+                      children: [],
+                    );
+                  }),
                 ),
               ),
             )
