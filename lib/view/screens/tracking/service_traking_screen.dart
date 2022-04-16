254d89b9461c40c65891a08c8b99421f6af60800
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2022-04-17 00:28:27 +0600

    working on design

--- lib/view/screens/tracking/service_traking_screen.dart
+++ lib/view/screens/tracking/service_traking_screen.dart
@@ -7,6 +7,7 @@ import 'package:get/get.dart';
 import '../../../constants/colors_data.dart';
 import '../../../constants/images.dart';
 import '../../../constants/size_config.dart';
+import '../../../constants/style_data.dart';
 import '../../../models/response/rp_server_model.dart';
 import 'widgets/service_time_line.dart';
 
@@ -72,30 +73,118 @@ class ServiceTrackingScreen extends StatelessWidget{
                   ],
                 ),
               ),
+            
               Expanded(
                 child: Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  width: SizeConfig.screenWidth,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).backgroundColor,
-                      borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(15.0),
-                          topLeft: Radius.circular(15.0)),
-                      shape: BoxShape.rectangle),
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                              width: SizeConfig.screenWidth,
-                              child: const ServiceTimeline(),),
-                          Container(),
-                        ],
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Stack(
+                    clipBehavior: Clip.antiAlias,
+                    children: [
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: SizedBox(
+                          width: SizeConfig.screenWidth,
+                          child: Card(
+                            color: Theme.of(context).errorColor,
+                            margin: EdgeInsets.zero,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10,),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    height: 50,
+                                    width: 50,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: const BoxDecoration(
+                                        color: kPrimaryColor, shape: BoxShape.circle),
+                                    child: Image.asset(
+                                      Images.service,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          service.title ?? '',
+                                          maxLines: 1,
+                                          style:
+                                          Theme.of(context).textTheme.subtitle1?.copyWith(
+                                            color: kSecondaryColor,
+                                          ),
+                                        ),
+                                        kHeightBox5,
+                                        Text(
+                                          'Car Details: ${service.subtitle ?? ''}',
+                                          textAlign: TextAlign.start,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1
+                                              ?.copyWith(
+                                            color:
+                                            Get.isDarkMode ? kWhiteColor : kBlackColor,
+                                          ),
+                                        ),
+                                        kHeightBox5,
+                                        Text(
+                                          'Date of service: ${service.date ?? ''}',
+                                          textAlign: TextAlign.start,
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyText1
+                                              ?.copyWith(
+                                            color:
+                                            Get.isDarkMode ? kWhiteColor : kBlackColor,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 60,),
+                        width: SizeConfig.screenWidth,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).cardColor,
+                            borderRadius: const BorderRadius.only(
+                                topRight: Radius.circular(15.0),
+                                topLeft: Radius.circular(15.0)),
+                            shape: BoxShape.rectangle),
+                        child: Stack(
+                          children: [
+
+                            SingleChildScrollView(
+                              child: Padding(
+                                padding:
+                                const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                                child: Column(
+                                  children: [
+                                    kHeightBox25,
+                                    kHeightBox25,
+                                    kHeightBox25,
+                                    SizedBox(
+                                      width: SizeConfig.screenWidth,
+                                      child: const ServiceTimeline(),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
