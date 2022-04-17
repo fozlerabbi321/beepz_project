f3b267a5b469be1bfc13466ebcd654e67bf551df
fozlerabbi321 <fozlerabbiinfo@gmail.com>
2022-04-17 16:32:04 +0600

    Positioned setup in top

--- lib/view/screens/tracking/service_traking_screen.dart
+++ lib/view/screens/tracking/service_traking_screen.dart
@@ -7,7 +7,6 @@ import 'package:get/get.dart';
 import '../../../constants/colors_data.dart';
 import '../../../constants/images.dart';
 import '../../../constants/size_config.dart';
-import '../../../constants/style_data.dart';
 import '../../../models/response/rp_server_model.dart';
 import 'widgets/service_time_line.dart';
 
@@ -28,167 +27,78 @@ class ServiceTrackingScreen extends StatelessWidget{
           color: Get.isDarkMode
               ? Theme.of(context).cardColor
               : kPrimaryColor,
-          child: Column(
-            children: [
-              Container(
-                color: Get.isDarkMode
-                    ? Theme.of(context).cardColor
-                    : kPrimaryColor,
-                //color: kPrimaryColor,
-                height: 58,
-                width: SizeConfig.screenWidth,
-                child: Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    Builder(
-                      builder: (cnx) => // Ensure Scaffold is in context
-                      InkWell(
-                        onTap: () {
-                          log('message');
-                         Get.back();
-
-                        },
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10,),
-                            child: SvgPicture.asset(
-                              Images.menu,
-                              color: kWhiteColor,
-                              height: 25,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: SizedBox(
-                        height: 35,
-                        child: Image.asset(
-                          Images.homeLogo,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(top: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  color: Get.isDarkMode
+                      ? Theme.of(context).cardColor
+                      : kPrimaryColor,
+                  //color: kPrimaryColor,
+                  height: 58,
+                  width: SizeConfig.screenWidth,
                   child: Stack(
-                    clipBehavior: Clip.antiAlias,
+                    clipBehavior: Clip.none,
                     children: [
-                      Align(
-                        alignment: Alignment.topCenter,
-                        child: SizedBox(
-                          width: SizeConfig.screenWidth,
-                          child: Card(
-                            color: Theme.of(context).errorColor,
-                            margin: EdgeInsets.zero,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20.0),
-                            ),
+                      Builder(
+                        builder: (cnx) => // Ensure Scaffold is in context
+                        InkWell(
+                          onTap: () {
+                            log('message');
+                           Get.back();
+
+                          },
+                          child: Align(
+                            alignment: Alignment.centerLeft,
                             child: Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10,),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 50,
-                                    width: 50,
-                                    padding: const EdgeInsets.all(10),
-                                    decoration: const BoxDecoration(
-                                        color: kPrimaryColor, shape: BoxShape.circle),
-                                    child: Image.asset(
-                                      Images.service,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          service.title ?? '',
-                                          maxLines: 1,
-                                          style:
-                                          Theme.of(context).textTheme.subtitle1?.copyWith(
-                                            color: kSecondaryColor,
-                                          ),
-                                        ),
-                                        kHeightBox5,
-                                        Text(
-                                          'Car Details: ${service.subtitle ?? ''}',
-                                          textAlign: TextAlign.start,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodyText1
-                                              ?.copyWith(
-                                            color:
-                                            Get.isDarkMode ? kWhiteColor : kBlackColor,
-                                          ),
-                                        ),
-                                        kHeightBox5,
-                                        Text(
-                                          'Date of service: ${service.date ?? ''}',
-                                          textAlign: TextAlign.start,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .bodyText1
-                                              ?.copyWith(
-                                            color:
-                                            Get.isDarkMode ? kWhiteColor : kBlackColor,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                              padding: const EdgeInsets.symmetric(horizontal: 10,),
+                              child: SvgPicture.asset(
+                                Images.menu,
+                                color: kWhiteColor,
+                                height: 25,
                               ),
                             ),
                           ),
                         ),
                       ),
-                      Container(
-                        padding: const EdgeInsets.only(top: 60,),
-                        width: SizeConfig.screenWidth,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).cardColor,
-                            borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(15.0),
-                                topLeft: Radius.circular(15.0)),
-                            shape: BoxShape.rectangle),
-                        child: Stack(
-                          children: [
-
-                            SingleChildScrollView(
-                              child: Padding(
-                                padding:
-                                const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                                child: Column(
-                                  children: [
-                                    kHeightBox25,
-                                    kHeightBox25,
-                                    kHeightBox25,
-                                    SizedBox(
-                                      width: SizeConfig.screenWidth,
-                                      child: const ServiceTimeline(),),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
+                      Center(
+                        child: SizedBox(
+                          height: 35,
+                          child: Image.asset(
+                            Images.homeLogo,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  padding: const EdgeInsets.only(top: 58),
+                  child: Container(
+                    padding: const EdgeInsets.only(top: 0,),
+                    width: SizeConfig.screenWidth,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(15.0),
+                            topLeft: Radius.circular(15.0)),
+                        shape: BoxShape.rectangle),
+                    child: Padding(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: SizeConfig.screenWidth,
+                            child:  ServiceTimeline(service: service,),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/view/screens/tracking/widgets/service_time_line.dart
+++ lib/view/screens/tracking/widgets/service_time_line.dart
@@ -2,41 +2,125 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:mystarter/constants/style_data.dart';
+import 'package:mystarter/models/response/rp_server_model.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 import '../../../../constants/colors_data.dart';
+import '../../../../constants/images.dart';
+import '../../../../constants/size_config.dart';
 import '../../../../models/response/rp_time_line_model.dart';
 
 class ServiceTimeline extends StatelessWidget {
+  final RpServerModel service;
   final int status;
 
   const ServiceTimeline({
     Key? key,
     this.status = 0,
+    required this.service,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: serverTimeList.length,
-        padding: EdgeInsets.zero,
-        shrinkWrap: true,
-        physics: const ScrollPhysics(),
-        itemBuilder: (context, index) {
-          return _buildTimelineTile(
-            indicator: _IconIndicator(
-              size: 20,
-              complete: status == 0 && index == 0 ? true : false,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [ Positioned(
+          top: -50,
+          child:  SizedBox(
+            width: SizeConfig.screenWidth,
+            child: Card(
+              color: Theme.of(context).cardColor,
+              margin: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10,),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                          color: kPrimaryColor, shape: BoxShape.circle),
+                      child: Image.asset(
+                        Images.service,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            service.title ?? '',
+                            maxLines: 1,
+                            style:
+                            Theme.of(context).textTheme.subtitle1?.copyWith(
+                              color: kSecondaryColor,
+                            ),
+                          ),
+                          kHeightBox5,
+                          Text(
+                            'Car Details: ${service.subtitle ?? ''}',
+                            textAlign: TextAlign.start,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                ?.copyWith(
+                              color:
+                              Get.isDarkMode ? kWhiteColor : kBlackColor,
+                            ),
+                          ),
+                          kHeightBox5,
+                          Text(
+                            'Date of service: ${service.date ?? ''}',
+                            textAlign: TextAlign.start,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText1
+                                ?.copyWith(
+                              color:
+                              Get.isDarkMode ? kWhiteColor : kBlackColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            complete: status == 0 && index == 0 ? true : false,
-            image: serverTimeList[index].image ?? '',
-            title: serverTimeList[index].title ?? '',
-            desc: serverTimeList[index].desc ?? '',
-            isFast: serverTimeList[index].isFast ?? false,
-            isLast: serverTimeList[index].isLast ?? false,
-            context: context,
-          );
-        });
+          ),),
+        Padding(
+          padding: const EdgeInsets.only(top: 35,),
+          child: ListView.builder(
+              itemCount: serverTimeList.length,
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemBuilder: (context, index) {
+                return _buildTimelineTile(
+                  indicator: _IconIndicator(
+                    size: 20,
+                    complete: status == 0 && index == 0 ? true : false,
+                  ),
+                  complete: status == 0 && index == 0 ? true : false,
+                  image: serverTimeList[index].image ?? '',
+                  title: serverTimeList[index].title ?? '',
+                  desc: serverTimeList[index].desc ?? '',
+                  isFast: serverTimeList[index].isFast ?? false,
+                  isLast: serverTimeList[index].isLast ?? false,
+                  context: context,
+                );
+              }),
+        ),
+      ],
+    );
   }
 
   TimelineTile _buildTimelineTile(
